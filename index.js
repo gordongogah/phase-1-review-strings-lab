387ad2b1221636e05f3a8b39674a8e3b872d83e5
gordongogah <gordongogah9@gmail.com>
2022-06-07 01:19:35 +0300

    commit tree 6872460a01d89044e08940b8e164a7b0168ec174

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Gordon'
+//let welcomeMessage = currentUser
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`;
+
+
+//console.log(excitedWelcomeMessage)
